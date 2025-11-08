5863ff4b53e73ecf451ddf6b2cdc844c3719d222
EC2 Default User <ec2-user@ip-172-31-86-148.ec2.internal>
2025-11-08 14:04:54 +0000

    Modify to add Mode of Payment

--- ury/ury/doctype/pos_invoice_list_view.js
+++ ury/ury/doctype/pos_invoice_list_view.js
@@ -1,6 +1,6 @@
-// Custom List View for POS Invoice to add Payment Type column
+// Custom List View for POS Invoice to add Mode of Payment column
 frappe.listview_settings['POS Invoice'] = {
-    add_fields: ["status", "grand_total"],
+    add_fields: ["status", "grand_total", "custom_payment_type"],
     
     get_indicator: function(doc) {
         if (doc.status === "Paid") {
@@ -15,58 +15,9 @@ frappe.listview_settings['POS Invoice'] = {
     },
     
     onload: function(listview) {
-        // Add custom column for Payment Type
+        // Add custom column for Mode of Payment
         listview.page.add_inner_button(__('Refresh'), function() {
             listview.refresh();
         });
-        
-        // Override the render method to add payment type column
-        const original_render = listview.render_list;
-        listview.render_list = function() {
-            original_render.call(this);
-            this.add_payment_type_column();
-        };
-        
-        listview.add_payment_type_column = function() {
-            const me = this;
-            // Get payment information for visible invoices
-            const invoice_names = this.data.map(d => d.name);
-            if (invoice_names.length > 0) {
-                frappe.call({
-                    method: 'frappe.client.get_list',
-                    args: {
-                        doctype: 'Sales Invoice Payment',
-                        filters: [['parent', 'in', invoice_names]],
-                        fields: ['parent', 'mode_of_payment'],
-                        limit_page_length: 1000
-                    },
-                    callback: function(r) {
-                        if (r.message) {
-                            const payment_map = {};
-                            r.message.forEach(payment => {
-                                if (!payment_map[payment.parent]) {
-                                    payment_map[payment.parent] = payment.mode_of_payment;
-                                }
-                            });
-                            
-                            // Add payment type to each row
-                            me.$result.find('.list-row').each(function() {
-                                const $row = $(this);
-                                const invoice_name = $row.find('[data-field="name"]').text().trim();
-                                const payment_type = payment_map[invoice_name] || '-';
-                                
-                                // Add payment type cell if not already added
-                                if (!$row.find('.payment-type-cell').length) {
-                                    const $payment_cell = $(`<div class="list-row-col ellipsis payment-type-cell" style="flex: 0 0 120px;">
-                                        <span class="indicator-pill ${payment_type !== '-' ? 'blue' : 'grey'}">${payment_type}</span>
-                                    </div>`);
-                                    $row.find('.list-row-col:last').after($payment_cell);
-                                }
-                            });
-                        }
-                    }
-                });
-            }
-        };
     }
 };
